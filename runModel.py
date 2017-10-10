13b872651f66da61587da3202dbdecf270941c96
Felipe Hernández <cephei.1313@gmail.com>
2017-10-10 16:36:17 -0300

    added other models

--- runModel.py
+++ runModel.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # model modules
-from models.speedModel import SM
 from models.SMinit import SMinit
+from models.FM import FM
+from models.EM_main import SMinit
 from models.test import Test
 
 import sys
@@ -12,23 +13,32 @@ import pickle
 def run_model(model_id, model_input_file_path, output_file_name):
     """ choose model and execute """
     
-    data_file = open(model_input_file_path, mode='rb')
-    input_dict = pickle.load(data_file)
-    data_file.close()
+    with open(model_input_file_path, mode='rb') as data_file:
+        input_dict = pickle.load(data_file)
+        # global properties given by user
+        first_input = input_dict["first_input"]
+        # output of previous models
+        second_input = input_dict["second_input"]
+
     output_dict = None
 
     if model_id == "S":
-        output_dict = SMinit(input_dict)
+        output_dict = SMinit(first_input)
     elif model_id == "F":
-        output_dict = SM(input_dict)
+        output_dict = FM(first_input, second_input)
     elif model_id == "E":
-        output_dict = SM(input_dict)
+        output_dict = EM_main(first_input, second_input)
     elif model_id == "T":
-        output_dict = SM(input_dict)
+        output_dict = FM(first_input, second_input)
     elif model_id == "Test":
         # for testing purpose
         output_dict = Test(input_dict)
     
+    output_dict = {
+        "first_input": first_input,
+        "second_input": output_dict,
+    }
+
     pickle_file = open(output_file_name, "wb")
     pickle.dump(output_dict, pickle_file, protocol=2)
     pickle_file.flush()
