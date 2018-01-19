af2e71fc4efd660e2fdc716c7e8a7a6d646ddbd1
Felipe Hernández <cephei.1313@gmail.com>
2018-01-19 20:37:06 -0300

    refactoring

--- tests/test.py
+++ tests/test.py
@@ -4,16 +4,32 @@ import unittest
 import pickle
 import os
 
+test_input_path = "tests/inputs/{0}"
+test_output_path = "tests/outputs/{0}"
+
 
 class BaseTest(unittest.TestCase):
     """ base to other tests """
 
     def __init__(self):
+        super(BaseTest, self).__init__()
         self.model_id = None
         self.input_file_path = None
         self.output_file_path = None
 
-        super(BaseTest, self).__init__()
+    def run_model(self):
+        """ run model and return dict """
+        run_model(self.model_id, self.input_file_path, self.output_file_path)
+
+        # check file
+        with open(self.output_file_path, mode='rb') as data_file:
+            input_dict = pickle.load(data_file)
+            # global properties given by user or previous model
+            model_input = input_dict["input"]
+            # output of previous models
+            model_output = input_dict["output"]
+
+        return model_input, model_output
 
     def delete_output_file(self):
         """ delete file """
@@ -24,22 +40,17 @@ class BaseTest(unittest.TestCase):
 class TestSpeedModel(BaseTest):
     """  run speed model """
 
-    def setUp(self):
+    def __init__(self):
+        super(TestSpeedModel, self).__init__()
         self.model_id = 'S'
-        self.input_file_path = "tests/speed.model_input"
-        self.output_file_path = "tests/aa.model_output"
+        self.input_file_path = test_input_path.format("speed.model_input")
+        self.output_file_path = test_output_path.format("speed.model_output")
 
     def test_run(self):
-        """  """
-        run_model(self.model_id, self.input_file_path, self.output_file_path)
+        """ validate output dict """
+        _, model_output = self.run_model()
 
-        # check file
-        with open(self.output_file_path, mode='rb') as data_file:
-            input_dict = pickle.load(data_file)
-            # global properties given by user
-            model_input = input_dict["input"]
-            # output of previous models
-            model_output = input_dict["output"]
+        # check dict answer here
 
         self.delete_output_file()
 
@@ -47,22 +58,17 @@ class TestSpeedModel(BaseTest):
 class TestForceModel(BaseTest):
     """  run force model """
 
-    def setUp(self):
+    def __init__(self):
+        super(TestForceModel, self).__init__()
         self.model_id = 'F'
-        self.input_file_path = "tests/speed.model_input"
-        self.output_file_path = "tests/speed.model_output"
+        self.input_file_path = test_input_path.format("force.model_input")
+        self.output_file_path = test_output_path.format("force.model_output")
 
     def test_run(self):
-        """  """
-        run_model(self.model_id, self.input_file_path, self.output_file_path)
+        """ validate output dict """
+        _, model_output = self.run_model()
 
-        # check file
-        with open(self.output_file_path, mode='rb') as data_file:
-            input_dict = pickle.load(data_file)
-            # global properties given by user
-            model_input = input_dict["input"]
-            # output of previous models
-            model_output = input_dict["output"]
+        # check dict answer here
 
         self.delete_output_file()
 
@@ -70,22 +76,17 @@ class TestForceModel(BaseTest):
 class TestEnergyModel(BaseTest):
     """  run energy model """
 
-    def setUp(self):
+    def __init__(self):
+        super(TestEnergyModel, self).__init__()
         self.model_id = 'E'
-        self.input_file_path = "tests/energy.model_input"
-        self.output_file_path = "tests/energy.model_output"
+        self.input_file_path = test_input_path.format("energy.model_input")
+        self.output_file_path = test_output_path.format("energy.model_output")
 
     def test_run(self):
-        """  """
-        run_model(self.model_id, self.input_file_path, self.output_file_path)
+        """ validate output dict """
+        _, model_output = self.run_model()
 
-        # check file
-        with open(self.output_file_path, mode='rb') as data_file:
-            input_dict = pickle.load(data_file)
-            # global properties given by user
-            model_input = input_dict["input"]
-            # output of previous models
-            model_output = input_dict["output"]
+        # check dict answer here
 
         self.delete_output_file()
 
@@ -93,22 +94,17 @@ class TestEnergyModel(BaseTest):
 class TestThermalModel(BaseTest):
     """  run thermal model """
 
-    def setUp(self):
+    def __init__(self):
+        super(TestThermalModel, self).__init__()
         self.model_id = 'T'
-        self.input_file_path = "tests/heat.model_input"
-        self.output_file_path = "tests/heat.model_output"
+        self.input_file_path = test_input_path.format("heat.model_input")
+        self.output_file_path = test_output_path.format("heat.model_output")
 
     def test_run(self):
-        """  """
-        run_model(self.model_id, self.input_file_path, self.output_file_path)
+        """ validate output dict """
+        _, model_output = self.run_model()
 
-        # check file
-        with open(self.output_file_path, mode='rb') as data_file:
-            input_dict = pickle.load(data_file)
-            # global properties given by user
-            model_input = input_dict["input"]
-            # output of previous models
-            model_output = input_dict["output"]
+        # check dict answer here
 
         self.delete_output_file()
 
