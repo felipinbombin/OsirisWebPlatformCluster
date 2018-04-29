5262491204e79b72295833e49415434ac46e5ad3
Felipe Hernández <cephei.1313@gmail.com>
2018-04-29 02:43:08 -0300

    added models path to sys.path

--- runModel.py
+++ runModel.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+import sys
+
+sys.path.append(os.path.join(os.getcwd(), 'models'))
+
 # model modules
 from models.SMinit import SMinit
 from models.FM import FM
@@ -9,7 +14,6 @@ from models.TM import TM
 from models.test import Test
 from models.ECM import energy_center_model
 
-import sys
 import pickle
 import gzip
 
