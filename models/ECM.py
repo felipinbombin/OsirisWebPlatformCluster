ad11f18fae76d6ef2c92fedf4b976c8d07622ca4
Felipe Hernández <cephei.1313@gmail.com>
2018-04-29 01:43:31 -0300

    give data from model input dict

--- models/ECM.py
+++ models/ECM.py
@@ -4,23 +4,28 @@ from . import RedLineas as line_network
 
 
 def energy_center_model(model_input, model_output):
+
+    datos_linea = model_input['ECM']['dc_data']
+    datos_cochrane = model_input['ECM']['ac_data']
+
     # Crear objeto linea
-    Linea1 = line_network.Linea('Linea1', DatosLinea1)
+    Linea1 = line_network.Linea('Linea1', datos_linea)
 
     # Crear objeto red AC
-    Cochrane = ac_network.RedAC('Cochrane', DatosCochrane)
+    Cochrane = ac_network.RedAC('Cochrane', datos_cochrane)
 
     # Conectar línea a red AC
     Cochrane.addLinea(Linea1)
 
     # Definir escenario de simulación con el diccionario creado anteriormente
-    Cochrane.DefinirSimulacion(DatosCochrane)
+    Cochrane.DefinirSimulacion(datos_cochrane)
 
     # Definir escenario de simulación con el diccionario creado anteriormente
-    Linea1.DefinirSimulacion(DatosLinea1)
+    Linea1.DefinirSimulacion(datos_linea)
 
     # Simular operación conjunto de red AC y línea de metro con potencia base Sbase = 1 [MW]
     Cochrane.simular(1)
 
     # Diccionario de resultados de las simulaciones
-    return Cochrane.saveresults()
+    model_output['ECM'] = Cochrane.saveresults()
+    return model_output
